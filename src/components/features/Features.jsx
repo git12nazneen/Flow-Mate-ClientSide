1ba93fccb2b27f3c5299894d6a6c48c2d05aebbe
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-10-31 13:57:01 +0600

    [prapty] ui changes

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -1,12 +1,12 @@
-import team from "../../assets/teamImg.jpg"; 
-import collab from "../../assets/teamCollaboration.jpg"; 
-import task from "../../assets/task management.jpg"; 
-import secure from "../../assets/secure.jpg"; 
-import success from "../../assets/sucess.jpg"; 
-import team1 from "../../assets/team.png"; 
-import team4 from "../../assets/team4.png"; 
-import team3 from "../../assets/team3.png"; 
-import team2 from "../../assets/team2.png"; 
+import team from "../../assets/teamImg.jpg";
+import collab from "../../assets/teamCollaboration.jpg";
+import task from "../../assets/task management.jpg";
+import secure from "../../assets/secure.jpg";
+import success from "../../assets/sucess.jpg";
+import team1 from "../../assets/team.png";
+import team4 from "../../assets/team4.png";
+import team3 from "../../assets/team3.png";
+import team2 from "../../assets/team2.png";
 
 import '../features/Animated.css'
 import Button2COmmon from "../button2Commo.jsx/Button2COmmon";
@@ -40,75 +40,75 @@ const Features = () => {
         </div>
         {/* Right Section */}
         <div className="relative w-[270px] lg:w-[420px] h-[300px] lg:h-[390px] mx-auto my-10 bg-gradient-to-r from-blue-200 via-gray-100 to-blue-100 rounded-full p-4 shadow-gray-900 shadow-2xl">
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-10 h-10 lg:w-20 lg:h-20 rounded-full bg-white flex items-center justify-center shadow-lg font-bold text-2xl z-10  p-10 border border-blue-400">
-       <div className="p-10 lg:p-24 border border-blue-600   rounded-full text-black">FM</div>
-      </div>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-10 h-10 lg:w-20 lg:h-20 rounded-full bg-white flex items-center justify-center shadow-lg font-bold text-2xl z-10  p-10 border border-blue-400">
+            <div className="p-10 lg:p-24 border border-blue-600   rounded-full text-black">FM</div>
+          </div>
 
-      {/* Rotating Container */}
-      <div className="absolute inset-0 animate-rotate-circle">
-         {/* Top Middle Image */}
-         <div className="absolute -top-2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-10 h-10 lg:w-20 lg:h-20 rounded-full bg-gray-100 flex items-center justify-center shadow-md">
-          <img
-            src={success}
-            alt="Team"
-            className="rounded-full w-10 h-10 lg:w-20 lg:h-20 counter-rotate bg-[#92e3ff]"
-          />
-        </div>
+          {/* Rotating Container */}
+          <div className="absolute inset-0 animate-rotate-circle">
+            {/* Top Middle Image */}
+            <div className="absolute -top-2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-10 h-10 lg:w-20 lg:h-20 rounded-full bg-gray-100 flex items-center justify-center shadow-md">
+              <img
+                src={success}
+                alt="Team"
+                className="rounded-full w-10 h-10 lg:w-20 lg:h-20 counter-rotate bg-[#92e3ff]"
+              />
+            </div>
 
-        {/* Top Right Image */}
-        <div className="absolute top-[10%] left-[80%] transform -translate-x-1/2 -translate-y-1/2 w-10 h-10 lg:w-20 lg:h-20 rounded-full bg-gray-100 flex items-center justify-center shadow-md">
-          <img
-            src={collab}
-            alt="Collab"
-            className="rounded-full w-10 h-10 lg:w-20 lg:h-20 counter-rotate bg-[#92e3ff]"
-          />
-        </div>
+            {/* Top Right Image */}
+            <div className="absolute top-[10%] left-[80%] transform -translate-x-1/2 -translate-y-1/2 w-10 h-10 lg:w-20 lg:h-20 rounded-full bg-gray-100 flex items-center justify-center shadow-md">
+              <img
+                src={collab}
+                alt="Collab"
+                className="rounded-full w-10 h-10 lg:w-20 lg:h-20 counter-rotate bg-[#92e3ff]"
+              />
+            </div>
 
-        {/* Right Middle Image */}
-        <div className="absolute top-1/2 left-[95%] transform -translate-x-1/2 -translate-y-1/2 w-10 h-10 lg:w-20 lg:h-20 rounded-full bg-gray-100 flex items-center justify-center shadow-md">
-          <img
-            src={team1}
-            alt="Team"
-            className="rounded-full w-10 h-10 lg:w-20 lg:h-20 counter-rotate bg-[#92e3ff]"
-          />
+            {/* Right Middle Image */}
+            <div className="absolute top-1/2 left-[95%] transform -translate-x-1/2 -translate-y-1/2 w-10 h-10 lg:w-20 lg:h-20 rounded-full bg-gray-100 flex items-center justify-center shadow-md">
+              <img
+                src={team1}
+                alt="Team"
+                className="rounded-full w-10 h-10 lg:w-20 lg:h-20 counter-rotate bg-[#92e3ff]"
+              />
+            </div>
+            <div className="absolute top-[80%] left-[85%] transform -translate-x-1/2 -translate-y-1/2 w-10 h-10 lg:w-20 lg:h-20 rounded-full bg-gray-100 flex items-center justify-center shadow-md">
+              <img
+                src={team2}
+                alt="Collab"
+                className="rounded-full w-10 h-10 lg:w-20 lg:h-20 counter-rotate bg-[#92e3ff]"
+              />
+            </div>
+            <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 w-10 h-10 lg:w-20 lg:h-20 rounded-full bg-gray-100 flex items-center justify-center shadow-md">
+              <img
+                src={secure}
+                alt="Secure"
+                className="rounded-full w-10 h-10 lg:w-20 lg:h-20 counter-rotate bg-[#92e3ff]"
+              />
+            </div>
+            <div className="absolute top-[80%] left-[15%] transform -translate-x-1/2 -translate-y-1/2 w-10 h-10 lg:w-20 lg:h-20 rounded-full bg-gray-100 flex items-center justify-center shadow-md">
+              <img
+                src={task}
+                alt="Task"
+                className="rounded-full w-10 h-10 lg:w-20 lg:h-20 counter-rotate bg-[#92e3ff]"
+              />
+            </div>
+            <div className="absolute top-1/2 left-0 transform -translate-x-1/2 -translate-y-1/2 w-10 h-10 lg:w-20 lg:h-20 rounded-full bg-gray-100 flex items-center justify-center shadow-md">
+              <img
+                src={team3}
+                alt="Task"
+                className="rounded-full w-10 h-10 lg:w-20 lg:h-20 counter-rotate bg-[#92e3ff]"
+              />
+            </div>
+            <div className="absolute top-[20%] left-[10%] transform -translate-x-1/2 -translate-y-1/2 w-10 h-10 lg:w-20 lg:h-20 rounded-full bg-gray-100 flex items-center justify-center shadow-md">
+              <img
+                src={team4}
+                alt="Secure"
+                className="rounded-full w-10 h-10 lg:w-20 lg:h-20 counter-rotate bg-[#92e3ff]"
+              />
+            </div>
+          </div>
         </div>
-        <div className="absolute top-[80%] left-[85%] transform -translate-x-1/2 -translate-y-1/2 w-10 h-10 lg:w-20 lg:h-20 rounded-full bg-gray-100 flex items-center justify-center shadow-md">
-          <img
-             src={team2}
-            alt="Collab"
-            className="rounded-full w-10 h-10 lg:w-20 lg:h-20 counter-rotate bg-[#92e3ff]"
-          />
-        </div>
-        <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 w-10 h-10 lg:w-20 lg:h-20 rounded-full bg-gray-100 flex items-center justify-center shadow-md">
-          <img
-            src={secure}
-            alt="Secure"
-            className="rounded-full w-10 h-10 lg:w-20 lg:h-20 counter-rotate bg-[#92e3ff]"
-          />
-        </div>
-        <div className="absolute top-[80%] left-[15%] transform -translate-x-1/2 -translate-y-1/2 w-10 h-10 lg:w-20 lg:h-20 rounded-full bg-gray-100 flex items-center justify-center shadow-md">
-        <img
-            src={task}
-            alt="Task"
-            className="rounded-full w-10 h-10 lg:w-20 lg:h-20 counter-rotate bg-[#92e3ff]" 
-          />
-        </div>
-        <div className="absolute top-1/2 left-0 transform -translate-x-1/2 -translate-y-1/2 w-10 h-10 lg:w-20 lg:h-20 rounded-full bg-gray-100 flex items-center justify-center shadow-md">
-          <img
-           src={team3}
-            alt="Task"
-            className="rounded-full w-10 h-10 lg:w-20 lg:h-20 counter-rotate bg-[#92e3ff]"
-          />
-        </div>
-        <div className="absolute top-[20%] left-[10%] transform -translate-x-1/2 -translate-y-1/2 w-10 h-10 lg:w-20 lg:h-20 rounded-full bg-gray-100 flex items-center justify-center shadow-md">
-        <img
-            src={team4}
-            alt="Secure"
-            className="rounded-full w-10 h-10 lg:w-20 lg:h-20 counter-rotate bg-[#92e3ff]"
-          />
-        </div>
-      </div>
-    </div>
       </div>
     </div>
   );
